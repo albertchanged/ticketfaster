c29ef95eaa13e576bdf43e4bc16daffe5a53657f
Albert Chang <albertchanged@gmail.com>
2018-01-04 18:32:11 -0800

    Add pretty good table layout for Event component

--- client/src/components/EventList.jsx
+++ client/src/components/EventList.jsx
@@ -4,7 +4,7 @@ import Event from './Event.jsx';
 const EventList = (props) => {
   return (
     <div>
-    <div>
+    {/* <div> */}
     {
       props.events.map((event) => (
         <Event 
@@ -13,14 +13,6 @@ const EventList = (props) => {
         />
       ))
     }
-    </div>
-      <div className="eventDiv">
-      <img src="https://s1.ticketm.net/dam/a/d48/7e9a0941-4272-40a1-aa78-cbd93f36fd48_525721_RETINA_PORTRAIT_3_2.jpg" className="favoriteImage"/>
-      <p><strong>Event:</strong> Fake Event<br /><strong>Venue:</strong> Fake Venue</p>
-      <p><strong>Description:</strong><br />adfasklfjdasl;kfjdslfkdsajflka;dsjf</p>
-      <p><strong>Date:</strong> 1/4/2018<br /><strong>Time:</strong> 20:08</p>
-      <button className="favoriteButton">I WANT TO GO</button>
-    </div>
     </div>
   )
 
