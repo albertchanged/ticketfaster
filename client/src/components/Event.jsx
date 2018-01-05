e6017dd06f47c92598e0d58fea659a41ad473f3c
Albert Chang <albertchanged@gmail.com>
2018-01-04 18:32:11 -0800

    Add pretty good table layout for Event component

--- client/src/components/Event.jsx
+++ client/src/components/Event.jsx
@@ -59,18 +59,28 @@ class Event extends React.Component {
     // console.log(this.state.favorited);
     return (
       <div className="eventDiv">
-        <div>
-        <p>Event: {this.props.event.name}</p>
-        </div>
-        <div>
-        <p>Venue: {this.props.event._embedded.venues[0].name + ', ' + this.props.event._embedded.venues[0].state.stateCode}</p>
-        </div>
-        <div>
+
+        <img src={this.props.event.images[4].url} className="favoriteImage"/>
+        <p>
+          <strong>Event:</strong><br /> {this.props.event.name}<br /><br />
+          <strong>Venue:</strong><br /> {this.props.event._embedded.venues[0].name + ', ' + this.props.event._embedded.venues[0].state.stateCode}
+        </p>
+        <p><strong>Description:</strong><br />{(this.props.event.info) ? this.props.event.info : 'No description available.'}</p>
+        <p><strong>Date:</strong><br /> 1/4/2018<br /><br /><strong>Time:</strong><br /> 20:08</p>
         <button className="favoriteButton" onClick={this.favoriteEvent.bind(this)}>I WANT TO GO</button>
-        </div>
+        
       </div>
     );
   }
 }
 
 export default Event;
+
+// </div>
+// <div className="eventDiv">
+// <img src="https://s1.ticketm.net/dam/a/d48/7e9a0941-4272-40a1-aa78-cbd93f36fd48_525721_RETINA_PORTRAIT_3_2.jpg" className="favoriteImage"/>
+// <p><strong>Event:</strong> Fake Event<br /><strong>Venue:</strong> Fake Venue</p>
+// <p><strong>Description:</strong><br />adfasklfjdasl;kfjdslfkdsajflka;dsjf</p>
+// <p><strong>Date:</strong> 1/4/2018<br /><strong>Time:</strong> 20:08</p>
+// <button className="favoriteButton">I WANT TO GO</button>
+// </div>
